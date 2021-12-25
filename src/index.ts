be1aa087d5cfb305efc0fa847ac66826cf8a10ad
Miikka Ylisiurunen <miikka.ylisiurunen@gmail.com>
2021-12-25 19:57:44 +0200

    Add router file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-import express, { Application, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import testRouter from './routes/testRouter';
 
-require('./database/mongoose');
-
-const app: Application = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use(testRouter);
 
 const PORT = process.env.PORT || 3001;
 
